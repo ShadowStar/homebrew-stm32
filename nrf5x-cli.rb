d76bb599fc7c80ee771303d2f1e15a908f233452
ShadowStar <orphen.leiliu@gmail.com>
2016-05-18 18:30:40 +0800

    Feature (nRF5x-Cli): Add new Formula for nRF5x Command Line Toolset

---
+++ nrf5x-cli.rb
@@ -0,0 +1,14 @@
+class Nrf5xCli < Formula
+
+  desc "Command Line Toolset for nRF5x"
+  homepage "http://www.nordicsemi.com/eng/Products/Bluetooth-Smart-Bluetooth-low-energy/nRF52832"
+  url "http://www.nordicsemi.com/eng/nordic/download_resource/53406/3/56966576"
+  version "8.4.0"
+  sha256 "1ee135c96dafa80d1c91c495dd3ad878ebabf3b453152d846d7a5acad6f06ad5"
+
+  def install
+    cp_r [ "mergehex", "nrfjprog" ], "#{prefix}/"
+    bin.install "mergehex/mergehex" => "mergehex"
+    bin.install "nrfjprog/nrfjprog" => "nrfjprog"
+  end
+end
