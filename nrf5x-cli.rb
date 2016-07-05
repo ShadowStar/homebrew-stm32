715d62fcdbfa63a9f2810f1063e2bda6d704e075
ShadowStar <orphen.leiliu@gmail.com>
2016-07-05 10:10:03 +0800

    Feature (nRF5x CLI): Update to version 9.0.0

--- nrf5x-cli.rb
+++ nrf5x-cli.rb
@@ -1,9 +1,9 @@
 class Nrf5xCli < Formula
   desc "Command Line Toolset for nRF5x"
   homepage "http://www.nordicsemi.com/eng/Products/Bluetooth-Smart-Bluetooth-low-energy/nRF52832"
-  url "http://www.nordicsemi.com/eng/nordic/download_resource/53406/4/75627120"
-  version "8.5.0"
-  sha256 "0823fd82c48ea1b6f37c1897b66dbf5e7ffd8d312e9c2d8b30169a6544af110b"
+  url "http://www.nordicsemi.com/eng/nordic/download_resource/53406/6/72310299"
+  version "9.0.0"
+  sha256 "9d568db6552d64eb65aa5d04241d0179fe67828e01b98c5c6de0760ad9993ef6"
 
   def install
     cp_r [ "mergehex", "nrfjprog" ], "#{prefix}/"
